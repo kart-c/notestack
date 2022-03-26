77172545d98637e9e684ec760ed7508cc7339e98
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-26 15:24:26 +0530

    feat: add auth on login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,57 @@
-import React from 'react';
+import { useState } from 'react';
+import { useAuth } from '../../Context';
+import { loginService } from '../../Services';
 import styles from './Login.module.css';
 
 const Login = ({ setModalState }) => {
+	const [loginData, setLoginData] = useState({ email: '', password: '' });
+
+	const { authDispatch } = useAuth();
+
+	const loginHandler = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await loginService(loginData, 'login');
+			if (response.status === 200) {
+				localStorage.setItem('token', response.data.encodedToken);
+				authDispatch({ type: 'AUTH', payload: response.data.encodedToken });
+				alert('Logged in!!');
+				setModalState('');
+			} else {
+				console.error('ERROR: ', response);
+				alert('ERROR');
+			}
+		} catch (error) {
+			console.error('ERROR: ', error);
+			alert('ERROR');
+		}
+	};
+
 	return (
 		<div className={styles.modal}>
 			<form>
 				<h3>Login</h3>
 				<div className={`input-container ${styles.inputContainer}`}>
 					<label htmlFor="email">Email Address </label>
-					<input type="email" id="email" name="email" placeholder="johndoe@gmail.com" />
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="johndoe@gmail.com"
+						value={loginData.email}
+						onChange={(e) => setLoginData((prev) => ({ ...prev, email: e.target.value }))}
+					/>
 				</div>
 				<div className={`input-container ${styles.inputContainer}`}>
 					<label htmlFor="password">Password </label>
-					<input type="password" id="password" name="password" placeholder="************" />
+					<input
+						type="password"
+						id="password"
+						name="password"
+						placeholder="************"
+						value={loginData.password}
+						onChange={(e) => setLoginData((prev) => ({ ...prev, password: e.target.value }))}
+					/>
 				</div>
 				<div className={styles.checkboxContainer}>
 					<div className="checkbox-container">
@@ -21,10 +60,20 @@ const Login = ({ setModalState }) => {
 					</div>
 					<button type="button">Forgot password?</button>
 				</div>
-				<button className={`btn ${styles.guestLogin}`} type="button">
+				<button
+					className={`btn ${styles.guestLogin}`}
+					type="button"
+					onClick={() =>
+						setLoginData((prev) => ({
+							...prev,
+							email: 'adarshbalika@gmail.com',
+							password: 'adarshBalika123',
+						}))
+					}
+				>
 					Guest Login
 				</button>
-				<button className={`btn ${styles.btn}`} type="submit">
+				<button className={`btn ${styles.btn}`} type="submit" onClick={loginHandler}>
 					Log In
 				</button>
 				<div className={styles.spacer}>
