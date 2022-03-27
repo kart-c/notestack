cdcf877e3ded06bcd8978baad1377fbe0f0d448f
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-27 21:39:02 +0530

    styles: refactor styles
    increase width of 1st aside and margin auto for whole page

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export { LoginModal } from './Modal/LoginModal';
 export { Login } from './Login/Login';
 export { Signup } from './Signup/Signup';
 export { Aside } from './Aside/Aside';
+export { LabelNotes } from './LabelNotes/LabelNotes';
