1e74718fc6a2faac65b8f029c6d5002664300f93
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-28 15:44:22 +0530

    feat: add quill editors

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import { contentModules, titleModules } from './quill.module';
+import 'react-quill/dist/quill.snow.css';
 import styles from './Editor.module.css';
+import './Quill.css';
 
 const Editor = () => {
+	const [noteTitle, setNoteTitle] = useState('');
+	const [noteContent, setNoteContent] = useState('');
+
+	const newNoteHandler = () => {};
+
 	return (
-		<div className={styles.editorSection}>
-			<div className={styles.editor}>
-				<h4>Note Title</h4>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam iure non doloremque
-					cupiditate! Adipisci in quasi laborum hic, reprehenderit labore debitis dolores sit
-					explicabo tempore numquam, sunt perspiciatis impedit maiores!
-				</p>
-			</div>
-		</div>
+		<section className={styles.editorSection}>
+			<h3 className={styles.editorTitle}>New Note Title</h3>
+			<ReactQuill
+				theme="snow"
+				value={noteTitle}
+				onChange={(e) => setNoteTitle(e)}
+				modules={titleModules}
+			/>
+			<h3 className={styles.editorTitle}>Content</h3>
+			<ReactQuill
+				theme="snow"
+				value={noteContent}
+				onChange={(e) => setNoteContent(e)}
+				modules={contentModules}
+			/>
+			<button className={`btn btn-primary ${styles.btn}`} onClick={newNoteHandler}>
+				Submit
+			</button>
+		</section>
 	);
 };
 
