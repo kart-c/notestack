5e336046fb626fb1768cef93cdc28283f6e2a2a6
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-28 18:10:54 +0530

    feat: post new note functionality

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import { contentModules, titleModules } from './quill.module';
 import 'react-quill/dist/quill.snow.css';
+import { addNewNote } from '../../Services';
+import { useAuth, useNotes } from '../../Context';
 import styles from './Editor.module.css';
 import './Quill.css';
 
@@ -9,7 +11,29 @@ const Editor = () => {
 	const [noteTitle, setNoteTitle] = useState('');
 	const [noteContent, setNoteContent] = useState('');
 
-	const newNoteHandler = () => {};
+	const {
+		authState: { token },
+	} = useAuth();
+
+	const { notesState, notesDispatch } = useNotes();
+
+	const newNoteHandler = async () => {
+		if (noteContent) {
+			const note = { noteTitle, noteContent };
+			try {
+				const response = await addNewNote(note, token);
+				if (response.status === 201) {
+					notesDispatch({ type: 'NEW_NOTE', payload: response.data.notes });
+				} else {
+					console.log('ERROR: ', response);
+				}
+			} catch (error) {
+				console.error('ERROR: ', error);
+			}
+		} else {
+			alert('Enter note content');
+		}
+	};
 
 	return (
 		<section className={styles.editorSection}>
--- src/Reducer/notesReducer.js
+++ src/Reducer/notesReducer.js
@@ -2,7 +2,6 @@ export const notesReducer = (state, action) => {
 	const { payload } = action;
 	switch (action.type) {
 		case 'NEW_NOTE':
-			console.log({ ...state, notes: payload });
 			return { ...state, notes: payload };
 
 		default:
