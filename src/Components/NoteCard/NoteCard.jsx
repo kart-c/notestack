626ff17daa11bd9b08bb3229c142a94644c4661a
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-29 21:19:41 +0530

    refactor: add noteCard

--- src/Components/NoteCard/NoteCard.jsx
+++ src/Components/NoteCard/NoteCard.jsx
@@ -7,14 +7,19 @@ import styles from './NoteCard.module.css';
 const NoteCard = ({ title, content, bgColor, _id }) => {
 	const navigate = useNavigate();
 
+	const parsedContent = HtmlParser(content);
+
+	const arrayOfContent = parsedContent.map((content) => [...content.props.children]);
+	const newContent = arrayOfContent.flat();
+
 	return (
 		<article
 			className={`card ${styles.card} ${bgColorCheck(bgColor)}`}
 			onClick={() => navigate(`/home/${_id}`)}
 		>
-			<div className={`content ${styles.content}`}>
-				{HtmlParser(title)}
-				{HtmlParser(content)}
+			<div className={`content ${styles.contentContainer}`}>
+				<div className={styles.title}>{HtmlParser(title)}</div>
+				<div className={styles.content}>{newContent.join(' ')}</div>
 			</div>
 		</article>
 	);
