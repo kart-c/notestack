73df5b5e531b332550a0083c080f6ef5da8757ee
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-30 15:04:50 +0530

    chore: add dummy notes

---
+++ src/Assets/dummy-notes.js
@@ -0,0 +1,17 @@
+export const dummyOne = {
+	bgColor: 'purple',
+	content:
+		"<p>Gathered together from the cosmic reaches of the universe, here in this great Hall of Justice, are the most powerful forces of good ever assembled: <strong>Superman!</strong> <strong>Batman</strong> and <strong>Robin!</strong> <strong>Wonder Woman!</strong> Aquaman! And The Wonder Twins: Zan and Jayna, with their space monkey, Gleek! Dedicated to prove justice and peace for all mankind!</p><p>Enter at your peril, past the vaulted door. Impossible things will happen that the world's never seen before. In Dexter's laboratory lives the smartest boy you've ever seen, but Dee Dee blows his experiments to Smithereens! There's gloom and doom when things go boom in Dexter's lab!</p><p>Yogi Bear is smarter than the average bear, Yogi Bear is always in the ranger's hair. At a picnic table you will find him there, stuffing down more goodies than the average bear. He will sleep till noon but before it's dark, he'll have every picnic basket that's in Jellystone Park. Yogi has it better than a millionaire. That's because he's smarter than the average bear!</p>",
+	date: new Date().toLocaleString(),
+	title: '<p>Dummy Note</p>',
+	_id: 'fa2eedf0-f4bf-4250-8932-182f61b59a62',
+};
+
+export const dummyTwo = {
+	bgColor: 'yellow',
+	content:
+		"<p>Spider-Man, Spider-Man</p><p>Does whatever a spider can</p><p>Spins a web, any size</p><p>Catches thieves just like flies</p><p>Look out</p><p>Here comes the Spider-Man</p><p>Is he strong?</p><p>Listen bud</p><p>He's got radioactive blood</p><p>Can he swing from a thread?</p><p>Take a look overhead</p><p>Hey, there</p><p>There goes the Spider-Man</p><p>In the chill of night</p><p>At the scene of a crime</p><p>Like a streak of light</p><p>He arrives just in time</p><p>Spider-Man, Spider-Man</p><p>Friendly neighborhood Spider-Man</p><p>Wealth and fame</p><p>He's ignored</p><p>Action is his reward</p><p>Look out</p><p>Here comes the Spider-Man</p><p>Spider-Man, Spider-Man</p><p>Friendly neighborhood Spider-Man</p><p>Wealth and fame</p><p>He's ignored</p><p>Action is his reward</p><p>Look out</p><p>Here comes the Spider-Man</p><p>In the chill of night</p><p>At the scene of a crime</p><p>Like a streak of light</p><p>He arrives just in time</p><p>Spider-Man, Spider-Man</p><p>Does whatever a spider can</p><p>Spins a web, any size</p><p>Catches thieves just like flies</p><p>Look out</p><p>Here comes the Spider-Man</p><p>Spider-Man, Spider-Man</p><p>Friendly neighborhood Spider-Man</p><p>Wealth and fame</p><p>He's ignored</p><p>Action is his reward</p><p>Look out</p><p>Here comes the Spider-Man</p>",
+	date: new Date().toLocaleString(),
+	title: '<p>The Spiderman Song</p>',
+	_id: '3de18364-f9b2-4b0f-a0be-058de4121efa',
+};
