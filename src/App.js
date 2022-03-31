8550fbec5cb2fa0ace5eac4193027b622d6f9a87
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-03-31 17:31:37 +0530

    refactor: keep common components in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home, Landing, SingleNotePage } from './Pages';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { Home, Landing, SingleNotePage, Archive } from './Pages';
 import Mockman from 'mockman-js';
-
+import { ArchiveNote, Aside, LabelNotes } from './Components';
 import './App.css';
 
 function App() {
+	const location = useLocation();
+
 	return (
-		<Routes>
-			<Route path="/" element={<Landing />} />
-			<Route path="/home" element={<Home />} />
-			<Route path="/mock" element={<Mockman />} />
-			<Route path="/home/:_id" element={<SingleNotePage />} />
-		</Routes>
+		<>
+			{location.pathname !== '/' ? (
+				<>
+					<Aside />
+					<LabelNotes />
+				</>
+			) : null}
+			<Routes>
+				<Route path="/" element={<Landing />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/mock" element={<Mockman />} />
+				<Route path="/home/:_id" element={<SingleNotePage />} />
+				<Route path="/archive" element={<Archive />} />
+				<Route path="/archive/:_id" element={<ArchiveNote />} />
+			</Routes>
+		</>
 	);
 }
 
