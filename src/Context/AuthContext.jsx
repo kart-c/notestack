b6c696962705bab54604eaecf9d9ff4469767dd9
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-02 22:29:40 +0530

    feat: persist user token on reload

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useReducer } from 'react';
+import { useEffect, createContext, useContext, useReducer } from 'react';
 import { authReducer } from '../Reducer';
 
 const AuthContext = createContext({
@@ -10,6 +10,13 @@ const AuthProvider = ({ children }) => {
 	const [authState, authDispatch] = useReducer(authReducer, {
 		token: '',
 	});
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		if (token) {
+			authDispatch({ type: 'AUTH', payload: token });
+		}
+	}, []);
 
 	return (
 		<AuthContext.Provider value={{ authState, authDispatch }}>{children}</AuthContext.Provider>
