2b04de1e66aa970d6b61ebae5e7df26daaf14488
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-02 22:25:12 +0530

    refactor: bg-color function with switch case

--- src/Utils/bgColorCheck.js
+++ src/Utils/bgColorCheck.js
@@ -1,12 +1,21 @@
-export const bgColorCheck = (bgColor) =>
-	bgColor === 'purple'
-		? 'note-purple'
-		: bgColor === 'red'
-		? 'note-red'
-		: bgColor === 'yellow'
-		? 'note-yellow'
-		: bgColor === 'green'
-		? 'note-green'
-		: bgColor === 'blue'
-		? 'note-blue'
-		: 'note-gray';
+export const bgColorCheck = (bgColor) => {
+	switch (bgColor) {
+		case 'purple':
+			return 'note-purple';
+
+		case 'red':
+			return 'note-red';
+
+		case 'yellow':
+			return 'note-yellow';
+
+		case 'green':
+			return 'note-green';
+
+		case 'blue':
+			return 'note-blue';
+
+		default:
+			return 'note-gray';
+	}
+};
