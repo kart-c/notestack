f44d2825439918501aeb3e6a23944e175dfe53c3
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-03 18:08:51 +0530

    feat: add new label

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Backdrop } from '../Backdrop/Backdrop';
+import { useLabel } from '../../Context';
 import styles from './Aside.module.css';
 
 const activeClass = ({ isActive }) => (isActive ? styles.active : '');
 
 const Aside = () => {
+	const [newLabel, setNewLabel] = useState('');
+	const [labelModal, setLabelModal] = useState(false);
+
+	const {
+		labelState: { labels },
+		labelDispatch,
+	} = useLabel();
+
+	const newLabelHandler = () => {
+		if (labels.some((label) => label === newLabel)) {
+			console.log('label already present');
+			setLabelModal(false);
+			setNewLabel('');
+		} else {
+			labelDispatch({ type: 'ADD_NEW_NOTE', payload: newLabel });
+			setLabelModal(false);
+			setNewLabel('');
+		}
+	};
+
 	return (
 		<aside className={styles.aside}>
+			{labelModal ? (
+				<>
+					<Backdrop setLabelModal={setLabelModal} />
+					<div className={styles.labelModal}>
+						<div className="input-container">
+							<label htmlFor="label">Add new Label</label>
+							<input
+								type="text"
+								id="label"
+								name="label"
+								placeholder="Travel"
+								value={newLabel}
+								onChange={(e) => setNewLabel(e.target.value)}
+							/>
+						</div>
+						<button className="btn btn-info btn-round" onClick={newLabelHandler}>
+							Add
+						</button>
+					</div>
+				</>
+			) : null}
 			<div className={styles.asideBtnList}>
 				<NavLink to="/home" className={activeClass}>
 					<i className="fa-solid fa-clipboard"></i> All notes
 				</NavLink>
 				<div className={styles.spacer}></div>
-				<button className={`btn btn-primary ${styles.newLabelBtn}`}>
+				{labels.length > 0
+					? labels.map((label) => (
+							<NavLink key={label} to={`/${label}`} className={activeClass}>
+								{label}
+							</NavLink>
+					  ))
+					: null}
+				<button
+					className={`btn btn-primary ${styles.newLabelBtn}`}
+					onClick={() => setLabelModal(true)}
+				>
 					<i className="fa-solid fa-circle-plus"></i> Add Label
 				</button>
 				<div className={styles.spacer}></div>
