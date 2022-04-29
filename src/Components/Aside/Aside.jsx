be085a86f1c5cbafd88b8f5fef702e828e8f66b0
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-29 22:38:04 +0530

    styles: aside module responsive styles

--- src/Components/Aside/Aside.jsx
+++ src/Components/Aside/Aside.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { LabelModal } from '../../Components';
-import { useAuth, useLabel } from '../../Context';
+import { useAuth, useFooterNav, useLabel } from '../../Context';
 import styles from './Aside.module.css';
 
 const activeClass = ({ isActive }) => (isActive ? styles.active : '');
@@ -21,6 +21,8 @@ const Aside = () => {
 
 	const navigate = useNavigate();
 
+	const { footerNav, setFooterNav } = useFooterNav();
+
 	const logoutHandler = () => {
 		toast.success('Logged out!');
 		localStorage.removeItem('token');
@@ -30,47 +32,53 @@ const Aside = () => {
 	};
 
 	return (
-		<aside className={styles.aside}>
-			{labelModal ? (
-				<>
-					<LabelModal setLabelModal={setLabelModal} />
-				</>
-			) : null}
-			<div className={styles.asideBtnList}>
-				<NavLink to="/home" className={activeClass}>
-					<i className="fa-solid fa-clipboard"></i> All notes
-				</NavLink>
-				<div className={styles.spacer}></div>
-				{labels.length > 0
-					? labels.map((label) => (
-							<NavLink key={label} to={`/${label}`} className={activeClass}>
-								<i className="fa-solid fa-tag"></i> {label}
-							</NavLink>
-					  ))
-					: null}
-				<button
-					className={`btn btn-primary ${styles.newLabelBtn}`}
-					onClick={() => setLabelModal(true)}
-				>
-					<i className="fa-solid fa-circle-plus"></i> Add Label
-				</button>
-				<div className={styles.spacer}></div>
-			</div>
-			<div className={styles.asideBtnList}>
-				<NavLink to="/archive" className={activeClass}>
-					<i className="fa-solid fa-box-archive"></i> Archive
-				</NavLink>
-				<NavLink to="/trash" className={activeClass}>
-					<i className="fa-solid fa-trash-can"></i> Trash
-				</NavLink>
-			</div>
-			<div className={styles.userProfile}>
-				<span>{user.firstName}</span>
-				<button className="btn btn-primary" onClick={logoutHandler} title="logout">
-					<i className="fa-solid fa-arrow-right-from-bracket"></i>
-				</button>
-			</div>
-		</aside>
+		<>
+			<div
+				className={footerNav.nav ? styles.backdrop : ''}
+				onClick={() => setFooterNav((prev) => ({ ...prev, nav: false }))}
+			></div>
+			<aside className={`${styles.aside} ${footerNav.nav ? styles.mobileAside : ''}`}>
+				{labelModal ? (
+					<>
+						<LabelModal setLabelModal={setLabelModal} />
+					</>
+				) : null}
+				<div className={styles.asideBtnList}>
+					<NavLink to="/home" className={activeClass}>
+						<i className="fa-solid fa-clipboard"></i> All notes
+					</NavLink>
+					<div className={styles.spacer}></div>
+					{labels.length > 0
+						? labels.map((label) => (
+								<NavLink key={label} to={`/${label}`} className={activeClass}>
+									<i className="fa-solid fa-tag"></i> {label}
+								</NavLink>
+						  ))
+						: null}
+					<button
+						className={`btn btn-primary ${styles.newLabelBtn}`}
+						onClick={() => setLabelModal(true)}
+					>
+						<i className="fa-solid fa-circle-plus"></i> Add Label
+					</button>
+					<div className={styles.spacer}></div>
+				</div>
+				<div className={`${styles.asideBtnList} ${styles.btnListTwo}`}>
+					<NavLink to="/archive" className={activeClass}>
+						<i className="fa-solid fa-box-archive"></i> Archive
+					</NavLink>
+					<NavLink to="/trash" className={activeClass}>
+						<i className="fa-solid fa-trash-can"></i> Trash
+					</NavLink>
+				</div>
+				<div className={styles.userProfile}>
+					<span>{user.firstName}</span>
+					<button className="btn btn-primary" onClick={logoutHandler} title="logout">
+						<i className="fa-solid fa-arrow-right-from-bracket"></i>
+					</button>
+				</div>
+			</aside>
+		</>
 	);
 };
 
