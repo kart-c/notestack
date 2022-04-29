d4e2db3498cf35c17654a0f800da95e9f8b0fafa
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-29 22:37:25 +0530

    styles: footer nav button handlers

--- src/Components/BtnFooter/FooterNav.jsx
+++ src/Components/BtnFooter/FooterNav.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useFooterNav } from '../../Context';
 import styles from './FooterNav.module.css';
 
 const FooterNav = () => {
+	const { setFooterNav } = useFooterNav();
+
 	return (
 		<div className={styles.btnContainer}>
-			<button className="btn btn-secondary">Navigation</button>
-			<button className="btn btn-secondary">Notes</button>
+			<button
+				className="btn btn-secondary"
+				onClick={() => setFooterNav((prev) => ({ ...prev, nav: true, labelNotes: false }))}
+			>
+				Navigation
+			</button>
+			<button
+				className="btn btn-secondary"
+				onClick={() => setFooterNav((prev) => ({ ...prev, nav: false, labelNotes: true }))}
+			>
+				Notes
+			</button>
 		</div>
 	);
 };
