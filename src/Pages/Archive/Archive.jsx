3dc086d3015aa89eeb5424433f0f4aaaed38d74e
Kartikeya Choudhary <kartikeyachoudhary11@gmail.com>
2022-04-30 17:34:42 +0530

    feat: show footernav in archive and trash

--- src/Pages/Archive/Archive.jsx
+++ src/Pages/Archive/Archive.jsx
@@ -1,9 +1,11 @@
+import { FooterNav } from '../../Components';
 import styles from './Archive.module.css';
 
 const Archive = () => {
 	return (
 		<>
 			<div className={styles.archiveContainer}></div>
+			<FooterNav />
 		</>
 	);
 };
--- src/Pages/Trash/Trash.jsx
+++ src/Pages/Trash/Trash.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { FooterNav } from '../../Components';
 import styles from './Trash.module.css';
 
 const Trash = () => {
-	return <div className={styles.trashContainer}></div>;
+	return (
+		<>
+			<div className={styles.trashContainer}></div>;
+			<FooterNav />
+		</>
+	);
 };
 
 export { Trash };
